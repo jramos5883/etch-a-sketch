d12a5b896f88f049304b4b4eefb29acb6a81a251
jramos5883 <jramos5883@gmail.com>
2022-09-12 13:17:33 -0700

    Worked on etch a sketch project, created buttons and grid, project is functional but needs to be styled

--- javascript.js
+++ javascript.js
@@ -1,1 +1,65 @@
 //comment
+let color = 'black';
+let click = true
+
+function populateBoard(size) {
+    let board = document.querySelector('.board');
+    let square = board.querySelectorAll('div');
+    square.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let amount = size * size;
+
+    for (let i = 0; i < amount; i++) {
+        let square = document.createElement('div');
+        square.addEventListener('mouseover', colorSquare);
+            square.style.backgroundColor = 'black';
+        square.style.backgroundColor = 'lightgrey';
+        board.insertAdjacentElement('beforeend', square);
+    }
+
+}
+
+populateBoard(16);
+
+function changeSize(input) {
+    if (input >= 2 && input <= 100) {
+        document.querySelector('.error').style.display = 'none'
+        populateBoard(input);
+    }
+    else {
+        document.querySelector('.error').style.display = 'flex';
+    }
+}
+
+function colorSquare() {
+    if (click) {
+    if (color === 'random') {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = color;
+    }
+}
+
+}
+
+function changeColor(choice) {
+    color = choice;
+}
+
+function resetBoard() {
+    let board = document.querySelector('.board');
+    let square = board.querySelectorAll('div');
+    square.forEach((div) => div.style.backgroundColor = 'lightgrey');
+}
+
+document.querySelector('body').addEventListener('click', (e) => {
+    if (e.target.tagName != 'BUTTON') {
+        click = !click;
+    if (click) {
+        document.querySelector('.mode').textContent = 'Mode: Coloring';
+    } else {
+        document.querySelector('.mode').textContent = 'Mode: Not Coloring';
+    }}
+});
